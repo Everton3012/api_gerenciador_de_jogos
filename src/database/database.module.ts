6f3f7c2a96eab804c9333fa07d4bb769f81a974b
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2025-12-08 12:07:53 -0300

    adiciona suporte ao TypeORM e configura módulo de banco de dados com conexão PostgreSQL

---
+++ src/database/database.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get<string>('DATABASE_URL'),
+        autoLoadEntities: true,
+        synchronize: configService.get<boolean>('DB_SYNCHRONIZE', true),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class DatabaseModule {}
