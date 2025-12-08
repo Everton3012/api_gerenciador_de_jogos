ae51a030923152d51f1a78c91f25fe5a1b2d72e4
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2025-12-08 12:49:18 -0300

    adiciona suporte ao Fastify e configura Swagger para documentação da API

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,38 @@
 import { NestFactory } from '@nestjs/core';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({ logger: true }),);
+
+  // Configuração do CORS
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN || '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
+  // Configuração do Swagger
+  const config = new DocumentBuilder()
+    .setTitle('API Gerenciador de Jogos')
+    .setDescription('API para gerenciar jogos')
+    .setVersion('1.0')
+    .addTag('games')
+    .addBearerAuth() // Se você usar autenticação JWT
+    .build();
+
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port, '0.0.0.0');
+  console.log(`Application is running on: ${await app.getUrl()}`);
+
+  console.log(`Swagger documentation: ${await app.getUrl()}/api`);
 }
 bootstrap();
